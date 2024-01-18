302e5baeaebc962eb299466b06323586c66eb09d
shiar18 <arshisingh1808@gmail.com>
2024-01-19 01:40:44 +0530

    "Updated SignIn"

--- lib/user/auth/signin.dart
+++ lib/user/auth/signin.dart
@@ -1,99 +1,217 @@
+// import 'package:flutter/material.dart';
+//
+// class SignIn extends StatefulWidget {
+// @override
+// _SignInState createState() => _SignInState();
+// }
+//
+// class _SignInState extends State<SignIn> {
+//   @override
+//   Widget build(BuildContext context) {
+//     double width=MediaQuery.of(context).size.width;
+//     double height=MediaQuery.of(context).size.height;
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Container(
+//         height: MediaQuery.of(context).size.height,
+//         width: double.infinity,
+//         alignment: Alignment.center,
+//         child: SingleChildScrollView(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Padding(
+//                 padding: const EdgeInsets.all(8.0),
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   crossAxisAlignment: CrossAxisAlignment.center,
+//                   children: [
+//                     IconButton(
+//                         icon: Icon(Icons.arrow_back_ios),
+//                         onPressed: (){
+//                           Navigator.of(context).pop();
+//                         }
+//                     ),
+//                     //Expanded(
+//                       Text('SignIn',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),
+//                             textAlign: TextAlign.center),
+//                     //)
+//                   ],
+//                 ),
+//               ),
+//               SizedBox(height: 30.0,),
+//               TextField(
+//                 decoration: InputDecoration(
+//                   hintText: 'Email',
+//                   suffixIcon: Icon(Icons.email),
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(20.0),
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(height: 20.0,),
+//               TextField(
+//                 obscureText: true,
+//                 decoration: InputDecoration(
+//                   hintText: 'Password',
+//                   suffixIcon: Icon(Icons.visibility_off),
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(20.0),
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(height: 30.0,),
+//               Padding(
+//                 padding: const EdgeInsets.all(10.0),
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Text('Forget password?',style: TextStyle(fontSize: 12.0),),
+//                     ElevatedButton(
+//                       child: const Text('Login'),
+//                       onPressed: () {},
+//                       style: ElevatedButton.styleFrom(
+//                         primary: Colors.blueAccent, // Background color
+//                         onPrimary: Colors.white
+//                       ),
+//                         //'Elevated Button',
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//               SizedBox(height:20.0),
+//               GestureDetector(
+//                 onTap: (){
+//                   //Navigator.push(context, MaterialPageRoute(builder: (context)=>Second()));
+//                 },
+//                 child: const Text.rich(
+//                   TextSpan(
+//                       text: 'Don\'t have an account?',
+//                       children: [
+//                         TextSpan(
+//                           text: '\t SignUp',
+//                           style: TextStyle(
+//                               color: Color(0xFF448AFF),
+//                           ),
+//                         ),
+//                       ]
+//                   ),
+//                 ),
+//               ),
+//
+//
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
 
-class SignIn extends StatefulWidget {
-@override
-_SignInState createState() => _SignInState();
-}
-
-class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    double width=MediaQuery.of(context).size.width;
-    double height=MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: Container(
-        height: height,
-        width: width,
-        child: SingleChildScrollView(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          margin: const EdgeInsets.all(24),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Container(
-                width: width,
-                height: height*0.45,
-                child: Image.asset('assets/images/education.png',fit: BoxFit.fill,),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text('Login',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
-                  ],
-                ),
-              ),
-              SizedBox(height: 30.0,),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  suffixIcon: Icon(Icons.email),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20.0,),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  suffixIcon: Icon(Icons.visibility_off),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                ),
-              ),
-              SizedBox(height: 30.0,),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Forget password?',style: TextStyle(fontSize: 12.0),),
-                    ElevatedButton(
-                      child: Text('Login'),
-                      onPressed: (){},
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height:20.0),
-              GestureDetector(
-                onTap: (){
-                  //Navigator.push(context, MaterialPageRoute(builder: (context)=>Second()));
-                },
-                child: Text.rich(
-                  TextSpan(
-                      text: 'Don\'t have an account',
-                      children: [
-                        TextSpan(
-                          text: 'Signup',
-                          style: TextStyle(
-                              color: Color(0xffEE7B23)
-                          ),
-                        ),
-                      ]
-                  ),
-                ),
-              ),
-
-
+              _header(context),
+              _inputField(context),
+              _forgotPassword(context),
+              _signup(context),
             ],
           ),
         ),
       ),
     );
   }
-}
 
+  _header(context) {
+    return const Column(
+      children: [
+        Text(
+          "Sign In",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        //Text("Enter your credential to login"),
+      ],
+    );
+  }
+
+  _inputField(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              hintText: "Username",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide.none
+              ),
+              fillColor: Colors.lightBlueAccent.withOpacity(0.1),
+              filled: true,
+              prefixIcon: const Icon(Icons.person)),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Password",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+            fillColor: Colors.lightBlueAccent.withOpacity(0.1),
+            filled: true,
+            prefixIcon: const Icon(Icons.password),
+          ),
+          obscureText: true,
+        ),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {
+          },
+          style: ElevatedButton.styleFrom(
+            shape: const StadiumBorder(),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blueAccent,
+          ),
+          child: const Text(
+            "Sign In",
+            style: TextStyle(fontSize: 20),
+          ),
+        )
+      ],
+    );
+  }
+
+  _forgotPassword(context) {
+    return TextButton(
+      onPressed: () {},
+      child: const Text("Forgot password?",
+        style: TextStyle(color: Colors.blueAccent),
+      ),
+    );
+  }
+
+  _signup(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account?"),
+        TextButton(
+            onPressed: () {
+            },
+            child: const Text("Sign Up", style: TextStyle(color: Colors.blueAccent),)
+        )
+      ],
+    );
+  }
+}
 
